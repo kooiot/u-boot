7a9d9ff81c5402a2204fef070ea46b96154f27f4
Dirk Chang <dirk@kooiot.com>
2026-09-05 23:33:17 +0800

    rockchip: port the ram mapping from master

--- arch/arm/mach-rockchip/rk3506/clk_rk3506.c
+++ arch/arm/mach-rockchip/rk3506/clk_rk3506.c
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright Contributors to the U-Boot project.
 
-#include <linux/kernel.h>
+#include <dm.h>
 #include <asm/arch-rockchip/cru_rk3506.h>
+
+int rockchip_get_clk(struct udevice **devp)
+{
+	return uclass_get_device_by_driver(UCLASS_CLK,
+				DM_DRIVER_GET(rockchip_rk3506_cru), devp);
+}
 
 void *rockchip_get_cru(void)
 {
